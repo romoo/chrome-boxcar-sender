935a53db255040d659b58ea811e0129009fdb468
Romoo <romoochen@gmail.com>
2012-10-09 23:19:45 +0800

    add notification support

--- js/boxcar.js
+++ js/boxcar.js
@@ -19,20 +19,57 @@ function save() {
   });
 }
 
-function boxcar(notification) {
+function notify(text) {
+  if(window.webkitNotifications) {
+    if(window.webkitNotifications.checkPermission() > 0) {
+      RequestPermission(notify);
+    } else {
+      var notification = webkitNotifications.createNotification(
+        './48.png',
+        'Sended',
+        text
+      );
+      notification.ondisplay = function() {
+        setTimeout('notification.cancel()', 5000);
+      };
+      notification.show();
+    }
+  }
+}
+
+function RequestPermission(callback) {
+  window.webkitNotifications.requestPermission(callback);
+}
+
+function boxcar(sendmessage) {
+  console.log('hah');
+  var message = (sendmessage ? sendmessage : document.getElementById('message').value);
+
   var req = new XMLHttpRequest();
   req.onreadystatechange = function() {
     // TODO: do sth here based on failure/success.
-    if (req.status == 200) {
-      console.log('Send it.');
+    if (req.readyState == 4){
+      // 4 = "loaded"
+      if (req.status == 200) {
+        console.log('Send it.');
+        // notify(message).show();
+        // background 不能触发 show() !?
+      }
+      else if(req.status == 400){
+        console.log('No application/event defined');
+      }
+      else if(req.status == 401){
+        console.log('Invalid username/password');
+      }
     }
   };
-  var message = (notification ? notification : document.getElementById('message').value);
-  chrome.storage.sync.get(["b_email","b_api_key"],function(date){
-    email= (date.b_email ? date.b_email : chrome.tabs.create({url: "options.html#email"}));
-    api_key = (date.b_api_key ? date.b_api_key : chrome.tabs.create({url: "options.html#email"}));
+  chrome.storage.sync.get(["b_email","b_api_key"], function(date){
+    // email= (date.b_email ? date.b_email : chrome.tabs.create({url: "options.html#email"}));
+    // api_key = (date.b_api_key ? date.b_api_key : chrome.tabs.create({url: "options.html#email"}));
+    email= (date.b_email ? date.b_email : window.open({url: "options.html#email"}));
+    api_key = (date.b_api_key ? date.b_api_key : window.open({url: "options.html#email"}));
     
-    var params = 'email=' + email + '&notification[from_screen_name]=Chrome' + '&notification[message]=' + message;
+    var params = 'email=' + email + '&notification[from_screen_name]=Chrome&notification[message]=' + message;
     req.open('POST', 'http://boxcar.io/devices/providers/' + api_key + '/notifications?' + params, true);
     req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     req.send(params);
@@ -41,9 +78,9 @@ function boxcar(notification) {
 }
 
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "hello") {
-      boxcar(request.text);
-    }
+function(request, sender, sendResponse) {
+  if(request.greeting == "hello") {
+    boxcar(request.text);
+    console.log(request.text);
   }
-);
+});
