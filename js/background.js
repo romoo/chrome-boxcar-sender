9e7c6902b802ba778361bfd0106f7a8cdcee6602
Romoo <romoochen@gmail.com>
2012-10-09 23:19:09 +0800

    change chrome.tabs.getSelected to chrome.tabs.query

--- js/background.js
+++ js/background.js
@@ -1,21 +1,16 @@
-/**
- * Returns a handler which will open a new window when activated.
- */
 function get_text(info, tab) {
   var text = info.selectionText;
-  chrome.tabs.getSelected(null, function (tab) {
-    chrome.tabs.sendMessage(tab.id, {"greeting": "hello", "text": text});
-    console.log('send');
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    chrome.tabs.sendMessage(tab.id, {
+      "greeting": "hello",
+      "text": text
+    });
   });
-  console.log(text);
 }
 
-/**
- * Create a context menu which will only show up for images.
- */
 chrome.contextMenus.create({
-  "type":"normal",
-  "title":"Send '%s' to Boxcar",
-  "contexts":["selection"],
+  "type": "normal",
+  "title": "Send '%s' to Boxcar",
+  "contexts": ["selection"],
   "onclick": get_text
 });
